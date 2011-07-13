513e76766f06efd70fca0e72ef56663869bd4629
n0iz <n0izeland@gmail.com>
2011-07-14 00:26:32 +0200

    Preferences überarbeitet.. löschen von Cookie

--- src/de/nware/app/hsDroid/ui/Preferences.java
+++ src/de/nware/app/hsDroid/ui/Preferences.java
@@ -8,10 +8,15 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.util.Log;
+import android.widget.Toast;
 import de.nware.app.hsDroid.R;
+import de.nware.app.hsDroid.data.StaticSessionData;
 import de.nware.app.hsDroid.provider.onlineService2Data.ExamsCol;
 
 public class Preferences extends PreferenceActivity {
+	private static final String TAG = "hsDroid-PreferenceActivity";
+	private Toast toast;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,7 +26,8 @@ public class Preferences extends PreferenceActivity {
 		customPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 
 			public boolean onPreferenceClick(Preference preference) {
-				startDialog();
+				showToast("Implement Me :)");
+				// startColorPickerDialog();
 				return true;
 			}
 
@@ -31,7 +37,29 @@ public class Preferences extends PreferenceActivity {
 		clearDBPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
 
 			public boolean onPreferenceClick(Preference preference) {
+				showToast("Datenbank gelöscht.");
 				clearDB();
+				return true;
+			}
+
+		});
+
+		Preference delCookiePref = (Preference) findPreference("delCookiePref");
+		delCookiePref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+			public boolean onPreferenceClick(Preference preference) {
+
+				if (StaticSessionData.cookies != null) {
+					// FIXME .clear() geht nicht..
+					// java.lang.UnsupportedOperationException warum??
+					// StaticSessionData.cookies.clear();
+					StaticSessionData.cookies = null;
+					showToast("Cookie gelöscht");
+				} else {
+					Log.d(TAG, "Versuch nicht vorhandenes Cookie zu lösche. Na na na, das geht aber nich.");
+					showToast("Kein Cookie vorhanden.");
+				}
+
 				return true;
 			}
 
@@ -56,7 +84,19 @@ public class Preferences extends PreferenceActivity {
 
 	}
 
-	private void startDialog() {
+	private void showToast(String text) {
+		// Toast erstellen oder ändern...
+		if (toast != null) {
+			toast.cancel();
+			toast.setText(text);
+			toast.show();
+		} else {
+			toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+			toast.show();
+		}
+	}
+
+	private void startColorPickerDialog() {
 		Dialog bla = new Dialog(this);
 		// TODO implement color picker
 		bla.setTitle("Implement Me");
